c206490f10cc92d1a23b454583f2f01a5c85fef5
rileyAdderley <104754955+rileyAdderley@users.noreply.github.com>
2022-07-25 22:01:20 +0930

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
-
+// the keyword extern means the function hello is defined in another file.
+// in our case, it is defined in the file function-1-1.cpp
+extern void hello();
 int main() {
-    std::cout<<"Object-Oriented Programming"<<std::endl;
-
+hello();
+return 0;
 }
+// in linux, you can compile this program using g++
+// g++
+//-Wall main-1-1.cpp function-1-1.cpp -o main.out
+//
+//-Wall means show all Warnings
+//I/
+//-o means you want the output to be named main.out
